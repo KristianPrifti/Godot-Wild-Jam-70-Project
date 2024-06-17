230934736bef0fd131b5459bcf60dd04c52ab1d8
Yakumo <bpl62@drexel.edu>
2024-06-17 12:29:55 -0400

    ghost hp stuff

--- GameJam/ghost.gd
+++ GameJam/ghost.gd
@@ -1,32 +1,66 @@
 extends CharacterBody2D
 
-
-
-var speed = 100
-
-var dead = false
-var player_chase = false
+'''Enemy movement varibles'''
+var speed = 200
+var player_in_range = false
 var player = null
 
+'''Enemy Combat Varible'''
+var health = 100
+var dead = false
+
+
 func _ready():
-	dead = false 
+	dead = false #sets the dead varibles to false as the game launches
 	
+'''Enemy Movement'''	
 func _physics_process(delta):
 	if !dead:
-		$"detection"/CollisionShape2D.disabled = false
-		if player_chase:
-			velocity = (player.get_global_position() - position).normalized() * speed * delta
+		$"detection"/CollisionShape2D.disabled = false #if the enemy is not dead, do not disable thier detection zone
+		if player_in_range: #if the player is in range of the enemy's detection zone.
+			velocity = (player.get_global_position() - position).normalized() * speed * delta 
+			#the enimes velocity is the player's position/direction - the enemies position/direction
+			#mutiplied by the speed of the enemy
+			$AnimatedSprite2D.play("Ghost_Chase")
 		else:
-			velocity = lerp(velocity, Vector2.ZERO, .1)
+			velocity = lerp(velocity, Vector2.ZERO, .07) #if the player is not in range, the velocity slowly drops to 0
+			$AnimatedSprite2D.play("Ghost_Chase")
 		move_and_collide(velocity)
 	if dead:
-		$"detection"/CollisionShape2D.disabled = true
+		$"detection"/CollisionShape2D.disabled = true #if dead, disable thier detection radius
 		
-func _on_detection_body_entered(body):
+'''Detections'''
+func _on_detection_body_entered(body): #checks if the player enters the detection zone
 	if body.has_method("player"):
-		player_chase = true
+		player_in_range = true
 		player = body
 
-func _on_detection_body_exited(body):
+func _on_detection_body_exited(body):  #checks if the player leaves the detection zone
 	if body.has_method("player"):
-		player_chase = false 
+		player_in_range = false 
+
+'''taking damage'''
+
+func _on_hitbox_area_entered(area):
+	var damage_dealt #initialized the damage varible
+	if area.has_method("player"): #checks if the area attacking has the player method
+		damage_dealt = 50 #sets the damage varible to 50
+		$AnimatedSprite2D.play("Damage")
+		take_damage(damage_dealt) #sends the damage dealt to the function for calculating damage
+
+func take_damage(damage):
+	health = health - damage #health is the current health - damage dealt
+	if health <= 0:
+		health = 0 #if health is less than or equal to 0, it is 0
+	
+	if health <= 0 and !dead:
+		death() #cals the death function
+
+func death(): # dies
+	dead = true
+	$AnimatedSprite2D.play("damage")
+	await get_tree().create_timer(1).timeout
+	queue_free()
+	
+func ghost():
+	pass
